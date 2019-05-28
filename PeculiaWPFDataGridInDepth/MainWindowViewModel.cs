6be2d532d14659619c9a90c232d8f35313775192
N Nemakhavhani <ndamulelo@bbd.co.za>
2019-05-28 15:13:56 +0200

    Add simple search

--- PeculiaWPFDataGridInDepth/MainWindowViewModel.cs
+++ PeculiaWPFDataGridInDepth/MainWindowViewModel.cs
@@ -5,15 +5,17 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace PeculiaWPFDataGridInDepth
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BindableBase
     {
         private List<TaxPayer> taxpayersList;
         public ListCollectionView collectionView;
         public GENDER currentGenderFilter = GENDER.FEMALE;
+        private string searchString = string.Empty;
 
         public MainWindowViewModel()
         {
@@ -41,7 +43,32 @@ namespace PeculiaWPFDataGridInDepth
         }
 
 
-        public GENDER GenderFilter { get { return currentGenderFilter; } set { } }
+        public GENDER GenderFilter
+        {
+            get { return currentGenderFilter; }
+            set
+            {
+                FilterGridCollection(value);
+                //MessageBox.Show($"Setting gender filter again:  {value}.");
+                SetProperty(ref currentGenderFilter, value);
+            }
+        }
+
+        public string SearchString
+        {
+            get
+            {
+                return searchString;
+            }
+
+            set
+            {
+                //MessageBox.Show($"New search term: {value}");
+                FilterByFirstName(value);
+                SetProperty(ref searchString, value);
+            }
+
+        }
 
 
 
@@ -50,6 +77,40 @@ namespace PeculiaWPFDataGridInDepth
             taxpayersList = DataService.GetAllTaxPayers();
             collectionView = new ListCollectionView(taxpayersList);
             collectionView.GroupDescriptions.Add(new PropertyGroupDescription("Gender"));
+        }
+
+        private void FilterByFirstName(string searchTerm)
+        {
+            if (collectionView == null) return;
+
+            collectionView.Filter = (user) =>
+            {
+                TaxPayer payer = user as TaxPayer;
+
+                if (payer.Firstname.Contains(searchTerm)) return true;
+
+                return false;
+            };
+        }
+
+        private void FilterGridCollection(GENDER filterVal)
+        {
+            if (collectionView != null)
+            {
+                collectionView.Filter = (user) =>
+                {
+                    TaxPayer payer = user as TaxPayer;
+
+                    if (payer.Gender == filterVal)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                };
+            }
         }
     }
 }
